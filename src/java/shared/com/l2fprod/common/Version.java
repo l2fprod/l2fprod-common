983aa91b4b78272d5ae67caefc5a08dd2b11c283
Frederic Lavigne <fred@L2FProd.com>
2018-07-01 11:00:50 +0200

    build

--- src/java/shared/com/l2fprod/common/Version.java
+++ src/java/shared/com/l2fprod/common/Version.java
@@ -1,5 +1,5 @@
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Sun Sep 07 18:25:23 CEST 2008 */
+/* Creation date Sun Jul 01 10:50:18 CEST 2018 */
 package com.l2fprod.common;
 
 import java.util.Date;
@@ -12,44 +12,14 @@ import java.util.Date;
 public class Version {
 
 
-   /** buildDate (set during build process to 1220804723056L). */
-   private static Date buildDate = new Date(1220804723056L);
+   /** buildDate (set during build process to 1530435018289L). */
+   private static Date buildDate = new Date(1530435018289L);
 
    /**
-    * Get buildDate (set during build process to Sun Sep 07 18:25:23 CEST 2008).
+    * Get buildDate (set during build process to Sun Jul 01 10:50:18 CEST 2018).
     * @return Date buildDate
     */
    public static final Date getBuildDate() { return buildDate; }
-
-
-   /** buildTimestamp (set during build process to "09/07/2008 06:25 PM"). */
-   private static String buildTimestamp = new String("09/07/2008 06:25 PM");
-
-   /**
-    * Get buildTimestamp (set during build process to "09/07/2008 06:25 PM").
-    * @return String buildTimestamp
-    */
-   public static final String getBuildTimestamp() { return buildTimestamp; }
-
-
-   /** year (set during build process to "2005-2007"). */
-   private static String year = new String("2005-2007");
-
-   /**
-    * Get year (set during build process to "2005-2007").
-    * @return String year
-    */
-   public static final String getYear() { return year; }
-
-
-   /** version (set during build process to "7.3"). */
-   private static String version = new String("7.3");
-
-   /**
-    * Get version (set during build process to "7.3").
-    * @return String version
-    */
-   public static final String getVersion() { return version; }
 
 
    /** project (set during build process to "l2fprod-common"). */
@@ -60,5 +30,35 @@ public class Version {
     * @return String project
     */
    public static final String getProject() { return project; }
+
+
+   /** year (set during build process to "2005-2009"). */
+   private static String year = new String("2005-2009");
+
+   /**
+    * Get year (set during build process to "2005-2009").
+    * @return String year
+    */
+   public static final String getYear() { return year; }
+
+
+   /** version (set during build process to "7.4-SNAPSHOT"). */
+   private static String version = new String("7.4-SNAPSHOT");
+
+   /**
+    * Get version (set during build process to "7.4-SNAPSHOT").
+    * @return String version
+    */
+   public static final String getVersion() { return version; }
+
+
+   /** buildTimestamp (set during build process to "07/01/2018 10:50 AM"). */
+   private static String buildTimestamp = new String("07/01/2018 10:50 AM");
+
+   /**
+    * Get buildTimestamp (set during build process to "07/01/2018 10:50 AM").
+    * @return String buildTimestamp
+    */
+   public static final String getBuildTimestamp() { return buildTimestamp; }
 
 }
